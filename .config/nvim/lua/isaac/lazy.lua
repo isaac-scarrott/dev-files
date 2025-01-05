9943c7efc4b7982f59367ed14b9b94e80bd324d3
isaac-scarrott <isaac.scarrott11@gmail.com>
2025-01-05 21:23:45 +0000

    Remove commented out Copilot plugin and CodeCompanion plugin
    - Removed the commented out Copilot plugin configuration.
    - Removed the CodeCompanion plugin and its dependencies.

--- .config/nvim/lua/isaac/lazy.lua
+++ .config/nvim/lua/isaac/lazy.lua
@@ -22,21 +22,6 @@ require("lazy").setup({
     { "christoomey/vim-tmux-navigator", lazy = false },
     "nvim-lua/plenary.nvim",
     "BurntSushi/ripgrep",
-    -- {
-    --   "github/copilot.vim",
-    --   event = "InsertEnter",
-    -- },
-    {
-      "olimorris/codecompanion.nvim",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
-        "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
-        { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves the default Neovim UI
-      },
-      config = true,
-    },
     "ggandor/lightspeed.nvim",
     -- { "vim-scripts/ReplaceWithRegister", lazy = false },
     -- END COMMON --
